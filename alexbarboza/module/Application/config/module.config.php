0aadbb6fad056203f2f9ae0c0abc6064410c7d31
assabarboza <alexplat11@hotmail.com>
2016-06-20 21:45:46 -0300

    Trabalho DSII

--- alexbarboza/module/Application/config/module.config.php
+++ alexbarboza/module/Application/config/module.config.php
@@ -10,6 +10,21 @@
 namespace Application;
 
 return array(
+    'doctrine' => array(
+        'driver' => array(
+            __NAMESPACE__ . '_driver' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => array(__DIR__ . '/../src/' . __NAMESPACE__ . '/Entity')
+            ),
+            'orm_default' => array(
+                'drivers' => array(
+                    __NAMESPACE__ . '\Entity' => __NAMESPACE__ . '_driver'
+                )
+            )
+        )  
+    ),
+    
     'router' => array(
         'routes' => array(
             'home' => array(
@@ -75,7 +90,8 @@ return array(
     ),
     'controllers' => array(
         'invokables' => array(
-            'Application\Controller\Index' => Controller\IndexController::class
+            'Application\Controller\Index' => Controller\IndexController::class,
+            'Application\Controller\Genero' => Controller\GeneroController::class
         ),
     ),
     'view_manager' => array(
